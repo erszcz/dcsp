6a580d84875ab14bba73dd900540b310f77ca017
Radosław Szymczyszyn <lavrin@gmail.com>
2013-02-06 15:23:22 +0100

    Add constraint checking for example problem #1

--- src/ex1.erl
+++ src/ex1.erl
@@ -1,13 +1,66 @@
 -module(ex1).
 
--export([init/2]).
+%% API
+-export([init/2,
+         is_consistent/2]).
 
 -include("dcsp.hrl").
+
+%% ------------------------------------------------------------------
+%% API
+%% ------------------------------------------------------------------
 
 init(AId, #problem{} = P) ->
     Domain = lists:nth(AId, P#problem.domains),
     Last = element(size(Domain), Domain),
     [{{x,AId}, Last}].
 
-what_now(AId, #problem{} = P, AgentView) ->
-    ok.
+is_consistent(AgentView, #problem{} = P) ->
+    VarView = agent_view_to_var_view(AgentView),
+    lists:all(mk_check_constraint(VarView), P#problem.constraints).
+
+%% ------------------------------------------------------------------
+%% Helpers
+%% ------------------------------------------------------------------
+
+agent_view_to_var_view(AgentView) ->
+    [ {{x,AId},Val} || {AId,Val} <- AgentView ].
+
+mk_check_constraint(VarView) ->
+    fun({Var1, Op, Var2}) ->
+            try
+                {Var1,Val1} = proplists:lookup(Var1, VarView),
+                {Var2,Val2} = proplists:lookup(Var2, VarView),
+                apply(fun erlang:Op/2, [Val1, Val2])
+            catch
+                error:{badmatch, none} ->
+                    true
+            end
+    end.
+
+%% ------------------------------------------------------------------
+%% Tests
+%% ------------------------------------------------------------------
+
+-ifdef(TEST).
+
+-include_lib("eunit/include/eunit.hrl").
+
+is_consistent_test_() ->
+    {setup,
+     fun () ->
+        #problem{constraints = [
+            {{x,1}, '/=', {x,3}},
+            {{x,2}, '/=', {x,3}}
+        ]}
+     end,
+     fun(Problem) -> [
+        ?_test(?assert(is_consistent([], Problem))),
+        ?_test(?assert(is_consistent([{1,1}, {3,2}], Problem))),
+        ?_test(?assert(is_consistent([{2,1}, {3,2}], Problem))),
+        ?_test(?assert(is_consistent([{1,1}, {2,2}], Problem))),
+        ?_test(?assertNot(is_consistent([{1,1}, {3,1}], Problem))),
+        ?_test(?assertNot(is_consistent([{2,1}, {3,1}], Problem)))
+     ] end}.
+
+-endif.
